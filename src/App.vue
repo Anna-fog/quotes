06860957ad00bb1b04ff72f9d6b69ac4bb5e180b
Anna <anna49.fog@yandex.ru>
2023-01-10 16:31:04 +0100

    feat: add tumbleweed. Update data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <template>
   <div class="app-wrapper">
     <Header/>
     <RouterView/>
+    <img
+      v-if="router.currentRoute.value.name === 'random'"
+      src="@/assets/images/tumbleweed.png"
+      class="tumbleweed"
+      alt="">
   </div>
 </template>
 
@@ -15,5 +23,33 @@ import Header from './components/Header.vue'
   color: var(--color-main-text);
   min-height: 100vh;
   background-color: var(--color-background);
+}
+
+.tumbleweed {
+  position: absolute;
+  left: -100px;
+  bottom: 0;
+  width: 100px;
+  height: auto;
+  animation: wind ease-in-out 5s;
+
+  @media (max-width: 560px) {
+    bottom: 50px;
+  }
+}
+
+@keyframes wind {
+  0% {
+    left: -100px;
+    transform: rotate(0);
+  }
+  99% {
+    display: block;
+  }
+  100% {
+    left: calc(100% + 100px);
+    transform: rotate(380deg);
+    display: none;
+  }
 }
 </style>
